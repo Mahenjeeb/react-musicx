637b4ffa300c3d4c06d384bda322e7011f5b8838
Mahenjeeb <mahenjeeb345@gmail.com>
2023-10-12 06:18:26 +0000

    Intial push test

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../components/NavBar.css'
+
+export default function NavBar() {
+    return (
+        <nav>
+            <h1 className='nav--logo'>Musicx</h1>
+        </nav>
+    )
+
+}
